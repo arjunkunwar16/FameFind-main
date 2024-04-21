daa5d967f2883fbfe4f5202037c6ff7f0d79b445
ArjunKunwar <arjunkunwar16aka@gmail.com>
2024-04-21 16:41:14 +0530

    many

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,6 +6,7 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -126,7 +127,7 @@ const Products = () => {
       <div className="container my-3 py-3">
         <div className="row">
           <div className="col-12">
-            <h2 className="display-5 text-center">Latest Products</h2>
+            <h2 className="display-5 text-center gradient-text">Latest Products</h2>
             <hr />
           </div>
         </div>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,29 +1,29 @@
 import React from "react";
+import './App.css';
 
 const Home = () => {
   return (
-    <>
-      <div className="hero border-1 pb-3">
-        <div className="card bg-dark text-white border-0 mx-3">
-          <img
-            className="card-img img-fluid"
-            src="./assets/main.png.jpg"
-            alt="Card"
-            height={500}
-          />
-          <div className="card-img-overlay d-flex align-items-center">
-            <div className="container">
-              <h1 className="card-title text fw-lighter">Struggling to find merch of your favorite celebrities? </h1>
-              <br />
-              <p className="card-text fs-5 d-none d-sm-block ">
-              Discover FameFinds: Your one-stop shop for all celebrity merch, saving you time and hassle!
-              </p>
-            </div>
+    <div className="hero border-1 pb-3">
+      <div className="card bg-dark text-white border-0 mx-3">
+        <img
+          className="card-img img-fluid"
+          src="./assets/main.png.jpg"
+          alt="Card"
+          height={500}
+        />
+        <div className="card-img-overlay d-flex align-items-center">
+          <div className="container">
+          <h1 className="ak">
+            Struggling to find merch of your favorite celebrities?
+          </h1>
+            <br />
+            <p className="card-text  d-none d-sm-block description-text">
+  Discover FameFinds: Your one-stop shop for all celebrity merch, saving you time and hassle!
+</p>
           </div>
         </div>
       </div>
-
-    </>
+    </div>
   );
 };
 
